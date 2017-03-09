ec3f271bb25653e5f3b131937e4a39c9e91122b3
otxserver-new <e_nunes@live.com>
2017-03-09 16:14:34 -0300

    fixed console erros

--- data/npc/lib/npcsystem/customModules.lua
+++ data/npc/lib/npcsystem/customModules.lua
@@ -211,26 +211,3 @@ function VoiceModule:callbackOnThink()
 	end
 	return true
 end
-
-local function Player.removeMoneyNpc(self, amount)
-	if not player:isPremium() then
-		return self:removeMoney(amount)
-	end
-
-	local moneyCount = self:getMoney()
-	local bankCount = self:getBankBalance()
-	if amount > moneyCount + bankCount then
-		return false
-	end
-
-	self:removeMoney(math.min(amount, moneyCount))
-	if amount > moneyCount then
-		self:setBankBalance(bankCount - math.max(amount - moneyCount, 0))
-		if moneyCount == 0 then
-			self:sendTextMessage(MESSAGE_INFO_DESCR, ("Paid %d gold from bank account. Your account balance is now %d gold."):format(amount, self:getBankBalance()))
-		else
-			self:sendTextMessage(MESSAGE_INFO_DESCR, ("Paid %d from inventory and %d gold from bank account. Your account balance is now %d gold."):format(moneyCount, amount - moneyCount, self:getBankBalance()))
-		end
-	end
-	return true
-end
--- data/spells/scripts/support/magic_shield.lua
+++ data/spells/scripts/support/magic_shield.lua
@@ -2,10 +2,8 @@ local combat = Combat()
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 combat:setParameter(COMBAT_PARAM_AGGRESSIVE, false)
 
-local condition = Condition(CONDITION_LIGHT)
-condition:setParameter(CONDITION_PARAM_LIGHT_LEVEL, 6)
-condition:setParameter(CONDITION_PARAM_LIGHT_COLOR, 215)
-condition:setParameter(CONDITION_PARAM_TICKS, (6 * 60 + 10) * 1000)
+local condition = Condition(CONDITION_MANASHIELD)
+condition:setParameter(CONDITION_PARAM_TICKS, 200000)
 combat:setCondition(condition)
 
 function onCastSpell(creature, variant)
